4a2b963d50da6f415c58fa8982a1d6e0449f264b
Piotr Roszatycki <dexter@debian.org>
2008-06-06 08:07:10 +0000

    * Autogen cleanup for fake subdirectory, too.

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-automake_version=1.10
+autogen () {
+    automake_version=1.10
 
-rm -f aclocal.m4 configure
+    rm -f aclocal.m4 configure
 
-aclocal-${automake_version} -I m4
-autoheader
-libtoolize --force --copy
-automake-${automake_version} --add-missing --copy
-autoconf
-rm -rf autom4te.cache
+    aclocal-${automake_version} -I m4
+    autoheader
+    libtoolize --force --copy
+    automake-${automake_version} --add-missing --copy
+    autoconf
+    rm -rf autom4te.cache
+    rm -f config.h.in~
+}
+
+cd $(dirname $0)
+autogen
+cd fake
+autogen
