b3a14d1431d650086d8e930998c18feef1e113cd
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2010-08-16 17:14:24 +0200

    Refactoring.

--- scripts/ldd.pl
+++ scripts/ldd.pl
@@ -9,6 +9,7 @@
 use strict;
 #use warnings;
 
+my @Libs = ();
 my %Libs = ();
 
 my $Status = 0;
@@ -16,138 +17,153 @@ my $Dynamic = 0;
 my $Format = '';
 
 my $Ldlinuxsodir = "/lib";
-my @Ld_library_path = qw(/usr/lib /lib /usr/lib64 /lib32 /usr/lib64 /lib64);
+my @Ld_Library_Path = qw(/usr/lib /lib /usr/lib32 /lib32 /usr/lib64 /lib64);
 
 
-sub ldso($) {
+sub ldso {
     my ($lib) = @_;
-    my @files = ();
+
+        return if $Libs{$lib};
 
     if ($lib =~ /^\//) {
         $Libs{$lib} = $lib;
-        push @files, $lib;
-    } else {
-        foreach my $ld_path (@Ld_library_path) {
-            next unless -f "$ld_path/$lib";
+        objdump($lib);
+        push @Libs, $lib;
+    }
+    else {
+        foreach my $dir (@Ld_Library_Path) {
+            next unless -f "$dir/$lib";
+
             my $badformat = 0;
-            open my $pipe, "objdump -p '$ld_path/$lib' 2>/dev/null |";
+            open my $pipe, "objdump -p '$dir/$lib' 2>/dev/null |";
             while (my $line = <$pipe>) {
                 if ($line =~ /file format (\S*)$/) {
-                    $badformat = 1 unless $Format eq $1;
+                    $badformat = 1 unless $1 eq $Format;
                     last;
                 }
             }
             close $pipe;
+
             next if $badformat;
-            $Libs{$lib} = "$ld_path/$lib";
-            push @files, "$ld_path/$lib";
+
+            $Libs{$lib} = "$dir/$lib";
+            objdump("$dir/$lib");
+            push @Libs, $lib;
+
+            last;
         }
-        objdump(@files);
     }
 }
 
 
-sub objdump(@) {
+sub objdump {
     my (@files) = @_;
-    my @libs = ();
 
     foreach my $file (@files) {
         open my $pipe, "objdump -p '$file' 2>/dev/null |";
         while (my $line = <$pipe>) {
             $line =~ s/^\s+//;
-            my @f = split (/\s+/, $line);
             if ($line =~ /file format (\S*)$/) {
                 if (not $Format) {
                     $Format = $1;
+                                foreach my $lib (split /:/, $ENV{LD_PRELOAD}) {
+                                        ldso($lib);
+                                }
                 } else {
                     next unless $Format eq $1;
                 }
             }
-            if (not $Dynamic and $f[0] eq "Dynamic") {
+            if (not $Dynamic and $line =~ /^Dynamic Section:/) {
                 $Dynamic = 1;
             }
-            next unless $f[0] eq "NEEDED";
-            if ($f[1] =~ /^ld-linux(\.|-)/) {
-                $f[1] = "$Ldlinuxsodir/" . $f[1];
+            next unless $line =~ /^ \s* NEEDED \s+ (.*) \s* $/x;
+            my $needed = $1;
+            if ($needed =~ /^ld-linux(\.|-)/) {
+                $needed = "$Ldlinuxsodir/$needed";
             }
-            if (not defined $Libs{$f[1]}) {
-                $Libs{$f[1]} = undef;
-                push @libs, $f[1];
-            }
+            ldso($needed);
         }
         close $pipe;
     }
+}
 
-    foreach my $lib (@libs) {
-        ldso($lib);
+sub load_ldsoconf {
+        my ($file) = @_;
+    open my ($fh), $file;
+    while (my $line = <$fh>) {
+        chomp $line;
+        $line =~ s/#.*//;
+        next if $line =~ /^\s*$/;
+
+                if ($line =~ /^include\s+(.*)\s*/) {
+                        my $include = $1;
+                        foreach my $incfile (glob $include) {
+                                load_ldsoconf($incfile);
+                        }
+                        next;
+                }
+
+        unshift @Ld_Library_Path, $line;
     }
+    close $fh;
 }
 
 MAIN: {
-    if (not @ARGV) {
+        my @args = @ARGV;
+
+    if (not @args) {
         print STDERR "fakeldd: missing file arguments\n";
         exit 1;
     }
-    
+
     if (not `which objdump`) {
         print STDERR "fakeldd: objdump: command not found: install binutils package\n";
         exit 1;
     }
-    
-    while ($ARGV[0] =~ /^-/) {
-        my $arg = $ARGV[0];
+
+    load_ldsoconf('/etc/ld.so.conf');
+    unshift @Ld_Library_Path, split(/:/, $ENV{LD_LIBRARY_PATH});
+
+    while ($args[0] =~ /^-/) {
+        my $arg = $args[0];
         shift @ARGV;
         last if $arg eq "--";
     }
-    
-    open my $fh, "/etc/ld.so.conf";
-    while (my $line = <$fh>) {
-        chomp $line;
-        unshift @Ld_library_path, $line;
-    }
-    close $fh;
-    
-    unshift @Ld_library_path, split(/:/, $ENV{LD_LIBRARY_PATH});
-    
-    foreach my $file (@ARGV) {
-        my $address;
+
+    foreach my $file (@args) {
         %Libs = ();
         $Dynamic = 0;
-    
-        if ($#ARGV > 0) {
+
+        if (@args > 1) {
             print "$file:\n";
         }
-    
+
         if (not -f $file) {
             print STDERR "ldd: $file: No such file or directory\n";
             $Status = 1;
             next;
         }
-    
+
         objdump($file);
-    
+
         if ($Dynamic == 0) {
             print "\tnot a dynamic executable\n";
             $Status = 1;
         } elsif (scalar %Libs eq "0") {
             print "\tstatically linked\n";
         }
-    
-        if ($Format =~ /^elf64-/) {
-            $address = "0x0000000000000000";
-        } else {
-            $address = "0x00000000";
-        }
-    
-        foreach my $lib (keys %Libs) {
+
+        my $address = '0x' . '0' x ($Format =~ /^elf64-/ ? 16 : 8);
+
+        foreach my $lib (@Libs) {
             if ($Libs{$lib}) {
                 printf "\t%s => %s (%s)\n", $lib, $Libs{$lib}, $address;
             } else {
                 printf "\t%s => not found\n", $lib;
             }
         }
-    
+
     }
-    
+
     exit $Status;
 }
