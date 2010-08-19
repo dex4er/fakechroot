a09c1b79abb4ae15323f1d290755671f7500c43c
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2010-08-19 22:03:53 +0200

    Reindent source.

--- src/libfakechroot.c
+++ src/libfakechroot.c
@@ -1450,113 +1450,104 @@ int euidaccess (const char *pathname, int mode)
 
 
 /* #include <unistd.h> */
-int execl (const char *path, const char *arg, ...)
-{
-  size_t argv_max = 1024;
-  const char **argv = alloca (argv_max * sizeof (const char *));
-  unsigned int i;
-  va_list args;
+int execl (const char *path, const char *arg, ...) {
+	size_t argv_max = 1024;
+	const char **argv = alloca(argv_max * sizeof(const char *));
+	unsigned int i;
+	va_list args;
 
-  argv[0] = arg;
+	argv[0] = arg;
 
-  va_start (args, arg);
-  i = 0;
-  while (argv[i++] != NULL)
-    {
-      if (i == argv_max)
-        {
-          const char **nptr = alloca ((argv_max *= 2) * sizeof (const char *));
+	va_start (args, arg);
+	i = 0;
+	while (argv[i++] != NULL) {
+		if (i == argv_max) {
+			const char **nptr = alloca((argv_max *= 2) * sizeof(const char *));
 
-            if ((char *) argv + i == (char *) nptr)
-            /* Stack grows up.  */
-            argv_max += i;
-          else
-            /* We have a hole in the stack.  */
-            argv = (const char **) memcpy (nptr, argv,
-                                           i * sizeof (const char *));
-        }
+			if ((char *) argv + i == (char *) nptr)
+				/* Stack grows up.  */
+				argv_max += i;
+			else
+				/* We have a hole in the stack.  */
+				argv = (const char **) memcpy(nptr, argv, i
+						* sizeof(const char *));
+		}
 
-      argv[i] = va_arg (args, const char *);
-    }
-  va_end (args);
+		argv[i] = va_arg (args, const char *);
+	}
+	va_end (args);
 
-  return execve (path, (char *const *) argv, environ);
+	return execve(path, (char * const *) argv, environ);
 }
 
 
 /* #include <unistd.h> */
-int execle (const char *path, const char *arg, ...)
-{
-  size_t argv_max = 1024;
-  const char **argv = alloca (argv_max * sizeof (const char *));
-  const char *const *envp;
-  unsigned int i;
-  va_list args;
-  argv[0] = arg;
+int execle (const char *path, const char *arg, ...) {
+	size_t argv_max = 1024;
+	const char **argv = alloca(argv_max * sizeof(const char *));
+	const char * const *envp;
+	unsigned int i;
+	va_list args;
+	argv[0] = arg;
 
-  va_start (args, arg);
-  i = 0;
-  while (argv[i++] != NULL)
-    {
-      if (i == argv_max)
-        {
-          const char **nptr = alloca ((argv_max *= 2) * sizeof (const char *));
+	va_start (args, arg);
+	i = 0;
+	while (argv[i++] != NULL) {
+		if (i == argv_max) {
+			const char **nptr = alloca((argv_max *= 2) * sizeof(const char *));
 
-            if ((char *) argv + i == (char *) nptr)
-            /* Stack grows up.  */
-            argv_max += i;
-          else
-            /* We have a hole in the stack.  */
-            argv = (const char **) memcpy (nptr, argv,
-                                           i * sizeof (const char *));
-        }
+			if ((char *) argv + i == (char *) nptr)
+				/* Stack grows up.  */
+				argv_max += i;
+			else
+				/* We have a hole in the stack.  */
+				argv = (const char **) memcpy(nptr, argv, i
+						* sizeof(const char *));
+		}
 
-      argv[i] = va_arg (args, const char *);
-    }
+		argv[i] = va_arg (args, const char *);
+	}
 
-  envp = va_arg (args, const char *const *);
-  va_end (args);
+	envp = va_arg (args, const char *const *);
+	va_end (args);
 
-  return execve (path, (char *const *) argv, (char *const *) envp);
+	return execve(path, (char * const *) argv, (char * const *) envp);
 }
 
 
 /* #include <unistd.h> */
-int execlp (const char *file, const char *arg, ...)
-{
-  size_t argv_max = 1024;
-  const char **argv = alloca (argv_max * sizeof (const char *));
-  unsigned int i;
-  va_list args;
-  char *fakechroot_path, fakechroot_buf[FAKECHROOT_MAXPATH];
+int execlp (const char *file, const char *arg, ...) {
+	size_t argv_max = 1024;
+	const char **argv = alloca(argv_max * sizeof(const char *));
+	unsigned int i;
+	va_list args;
+	char *fakechroot_path, fakechroot_buf[FAKECHROOT_MAXPATH];
 
+	argv[0] = arg;
 
-  argv[0] = arg;
+	va_start (args, arg);
+	i = 0;
+	while (argv[i++] != NULL) {
+		if (i == argv_max) {
+			const char **nptr = alloca((argv_max *= 2) * sizeof(const char *));
 
-  va_start (args, arg);
-  i = 0;
-  while (argv[i++] != NULL)
-    {
-      if (i == argv_max)
-        {
-          const char **nptr = alloca ((argv_max *= 2) * sizeof (const char *));
+			if ((char *) argv + i == (char *) nptr)
+				/* Stack grows up.  */
+				argv_max += i;
+			else
+				/* We have a hole in the stack.  */
+				argv = (const char **) memcpy(nptr, argv, i
+						* sizeof(const char *));
+		}
 
-            if ((char *) argv + i == (char *) nptr)
-            /* Stack grows up.  */
-            argv_max += i;
-          else
-            /* We have a hole in the stack.  */
-            argv = (const char **) memcpy (nptr, argv,
-                                           i * sizeof (const char *));
-        }
+		argv[i] = va_arg (args, const char *);
+	}
+	va_end (args);
 
-      argv[i] = va_arg (args, const char *);
-    }
-  va_end (args);
-
-  expand_chroot_path(file, fakechroot_path, fakechroot_buf);
-  if (next_execvp == NULL) fakechroot_init();
-  return next_execvp (file, (char *const *) argv);
+	expand_chroot_path(file, fakechroot_path, fakechroot_buf);
+	if (next_execvp == NULL)
+		fakechroot_init();
+	return next_execvp(file, (char * const *) argv);
 }
 
 
@@ -1572,8 +1563,7 @@ int execv (const char *path, char *const argv [])
  *
  * FAKECHROOT_CMD_SUBST=cmd=subst:cmd=subst:...
  */
-static int
-try_cmd_subst (char *env, const char *filename, char *cmd_subst)
+static int try_cmd_subst (char *env, const char *filename, char *cmd_subst)
 {
     int len = strlen (filename), len2;
     char *p;
@@ -1730,98 +1720,89 @@ int execve (const char *filename, char *const argv [], char *const envp[])
 
 
 /* #include <unistd.h> */
-int execvp (const char *file, char *const argv [])
-{
-  if (*file == '\0')
-    {
-      /* We check the simple case first. */
-      errno = ENOENT;
-      return -1;
-    }
+int execvp (const char *file, char * const argv[]) {
+	if (*file == '\0') {
+		/* We check the simple case first. */
+		errno = ENOENT;
+		return -1;
+	}
 
-  if (strchr (file, '/') != NULL)
-    {
-      /* Don't search when it contains a slash.  */
-      return execve (file, argv, environ);
-    }
-  else
-    {
-      int got_eacces = 0;
-      char *path, *p, *name;
-      size_t len;
-      size_t pathlen;
+	if (strchr(file, '/') != NULL) {
+		/* Don't search when it contains a slash.  */
+		return execve(file, argv, environ);
+	} else {
+		int got_eacces = 0;
+		char *path, *p, *name;
+		size_t len;
+		size_t pathlen;
 
-      path = getenv ("PATH");
-      if (path == NULL)
-        {
-          /* There is no `PATH' in the environment.
-             The default search path is the current directory
-             followed by the path `confstr' returns for `_CS_PATH'.  */
-          len = confstr (_CS_PATH, (char *) NULL, 0);
-          path = (char *) alloca (1 + len);
-          path[0] = ':';
-          (void) confstr (_CS_PATH, path + 1, len);
-        }
+		path = getenv("PATH");
+		if (path == NULL) {
+			/* There is no `PATH' in the environment.
+			 The default search path is the current directory
+			 followed by the path `confstr' returns for `_CS_PATH'.  */
+			len = confstr(_CS_PATH, (char *) NULL, 0);
+			path = (char *) alloca(1 + len);
+			path[0] = ':';
+			(void) confstr(_CS_PATH, path + 1, len);
+		}
 
-      len = strlen (file) + 1;
-      pathlen = strlen (path);
-      name = alloca (pathlen + len + 1);
-      /* Copy the file name at the top.  */
-      name = (char *) memcpy (name + pathlen + 1, file, len);
-      /* And add the slash.  */
-      *--name = '/';
+		len = strlen(file) + 1;
+		pathlen = strlen(path);
+		name = alloca(pathlen + len + 1);
+		/* Copy the file name at the top.  */
+		name = (char *) memcpy(name + pathlen + 1, file, len);
+		/* And add the slash.  */
+		*--name = '/';
 
-      p = path;
-      do
-        {
-          char *startp;
+		p = path;
+		do {
+			char *startp;
 
-          path = p;
-          p = strchrnul (path, ':');
+			path = p;
+			p = strchrnul(path, ':');
 
-          if (p == path)
-            /* Two adjacent colons, or a colon at the beginning or the end
-               of `PATH' means to search the current directory.  */
-            startp = name + 1;
-          else
-            startp = (char *) memcpy (name - (p - path), path, p - path);
+			if (p == path)
+				/* Two adjacent colons, or a colon at the beginning or the end
+				 of `PATH' means to search the current directory.  */
+				startp = name + 1;
+			else
+				startp = (char *) memcpy(name - (p - path), path, p - path);
 
-          /* Try to execute this name.  If it works, execv will not return.  */
-          execve (startp, argv, environ);
+			/* Try to execute this name.  If it works, execv will not return.  */
+			execve(startp, argv, environ);
 
-          switch (errno)
-            {
-            case EACCES:
-              /* Record the we got a `Permission denied' error.  If we end
-                 up finding no executable we can use, we want to diagnose
-                 that we did find one but were denied access.  */
-              got_eacces = 1;
-            case ENOENT:
-            case ESTALE:
-            case ENOTDIR:
-              /* Those errors indicate the file is missing or not executable
-                 by us, in which case we want to just try the next path
-                 directory.  */
-              break;
+			switch (errno) {
+			case EACCES:
+				/* Record the we got a `Permission denied' error.  If we end
+				 up finding no executable we can use, we want to diagnose
+				 that we did find one but were denied access.  */
+				got_eacces = 1;
+			case ENOENT:
+			case ESTALE:
+			case ENOTDIR:
+				/* Those errors indicate the file is missing or not executable
+				 by us, in which case we want to just try the next path
+				 directory.  */
+				break;
 
-            default:
-              /* Some other error means we found an executable file, but
-                 something went wrong executing it; return the error to our
-                 caller.  */
-              return -1;
-            }
-        }
-      while (*p++ != '\0');
+			default:
+				/* Some other error means we found an executable file, but
+				 something went wrong executing it; return the error to our
+				 caller.  */
+				return -1;
+			}
+		} while (*p++ != '\0');
 
-      /* We tried every element and none of them worked.  */
-      if (got_eacces)
-        /* At least one failure was due to permissions, so report that
-           error.  */
-        errno = EACCES;
-    }
+		/* We tried every element and none of them worked.  */
+		if (got_eacces)
+			/* At least one failure was due to permissions, so report that
+			 error.  */
+			errno = EACCES;
+	}
 
-  /* Return the error from the last attempt (probably ENOENT).  */
-  return -1;
+	/* Return the error from the last attempt (probably ENOENT).  */
+	return -1;
 }
 
 
