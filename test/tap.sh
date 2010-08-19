4d5e8e31a70e720beedeff44fce61dcbaa0ac236
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2010-08-19 11:27:27 +0200

    Prefix tap_ for TAP shell functions.

--- test/tap.sh
+++ test/tap.sh
@@ -1,28 +1,28 @@
 
 plan () {
-    ret=0
-    n=1
+    tap_ret=0
+    tap_n=1
     echo "1..$1"
 }
 
 not () {
     printf "not "
-    ret=1
+    tap_ret=1
 }
 
 ok () {
-    echo "ok $n $@"
-    n=$(($n+1))
+    echo "ok $tap_n $@"
+    tap_n=$(( $tap_n + 1 ))
 }
 
 skip () {
-    skip_n=$1
+    tap_skip_n=$1
     shift
-    for skip_i in `seq 1 $skip_n`; do
+    for tap_skip_i in `seq 1 $tap_skip_n`; do
         ok "# SKIP $@"
     done
 }
 
 end () {
-    exit $ret
+    exit $tap_ret
 }
