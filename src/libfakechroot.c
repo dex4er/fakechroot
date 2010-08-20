0c87de43c9499ffc40d1cf05ce8baa4f09c1c56a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2010-08-21 01:05:01 +0200

    New function system(3) for GNU libc.

--- src/libfakechroot.c
+++ src/libfakechroot.c
@@ -44,6 +44,7 @@
 #include <glob.h>
 #include <utime.h>
 #include <pwd.h>
+#include <signal.h>
 #ifdef HAVE_LIBINTL_H
 #include <libintl.h>
 #endif
@@ -56,6 +57,7 @@
 #ifdef HAVE_SHADOW_H
 #include <shadow.h>
 #endif
+#include <sys/wait.h>
 #ifdef HAVE_SYS_INOTIFY_H
 #include <sys/inotify.h>
 #endif
@@ -539,6 +541,7 @@ static int     (*next_stat64) (const char *file_name, struct stat64 *buf) = NULL
 #endif
 #endif
 static int     (*next_symlink) (const char *oldpath, const char *newpath) = NULL;
+/* static int     (*next_system) (const char *command) = NULL; */
 static char *  (*next_tempnam) (const char *dir, const char *pfx) = NULL;
 static char *  (*next_tmpnam) (char *s) = NULL;
 static int     (*next_truncate) (const char *path, off_t length) = NULL;
@@ -832,6 +835,7 @@ void fakechroot_init (void)
 #endif
 #endif
     nextsym(symlink, "symlink");
+/*    nextsym(symlink, "system"); */
     nextsym(tempnam, "tempnam");
     nextsym(tmpnam, "tmpnam");
     nextsym(truncate, "truncate");
@@ -2948,6 +2952,54 @@ int symlink (const char *oldpath, const char *newpath)
     if (next_symlink == NULL) fakechroot_init();
     return next_symlink(oldpath, newpath);
 }
+
+
+#ifdef __GNUC__
+/*
+   system reimplementation taken from bionic. glibc uses internal weak aliases
+   which cannot be overriden.
+ */
+/* #include <sys/types.h> */
+/* #include <signal.h> */
+/* #include <stdlib.h> */
+/* #include <unistd.h> */
+/* #include <sys/wait.h> */
+int system (const char *command)
+{
+    pid_t pid;
+    sig_t intsave, quitsave;
+    sigset_t mask, omask;
+    int pstat;
+    char *argp[] = {"sh", "-c", NULL, NULL};
+
+    if (!command) /* just checking... */
+    return(1);
+
+    argp[2] = (char *)command;
+
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &mask, &omask);
+    switch (pid = vfork()) {
+        case -1: /* error */
+        sigprocmask(SIG_SETMASK, &omask, NULL);
+        return(-1);
+        case 0: /* child */
+        sigprocmask(SIG_SETMASK, &omask, NULL);
+        execve("/bin/sh", argp, environ);
+        _exit(127);
+    }
+
+    intsave = (sig_t) bsd_signal(SIGINT, SIG_IGN);
+    quitsave = (sig_t) bsd_signal(SIGQUIT, SIG_IGN);
+    pid = waitpid(pid, (int *)&pstat, 0);
+    sigprocmask(SIG_SETMASK, &omask, NULL);
+    (void)bsd_signal(SIGINT, intsave);
+    (void)bsd_signal(SIGQUIT, quitsave);
+    return (pid == -1 ? -1 : pstat);
+
+}
+#endif
 
 
 /* #include <stdio.h> */
