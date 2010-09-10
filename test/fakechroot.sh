18cdae68429db7a0c7f730adeac5f6a58b7cd174
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2010-09-10 22:20:54 +0200

    More portable test scripts

--- test/fakechroot.sh
+++ test/fakechroot.sh
@@ -3,18 +3,4 @@
 srcdir=${srcdir:-.}
 . $srcdir/common.inc
 
-prepare_env
-
-if [ $# -gt 0 ]; then
-    destdir=$1
-    shift
-else
-    destdir=testtree
-fi
-
-if [ $# -gt 0 ]; then
-    HOME=/root $destdir/usr/sbin/chroot `pwd -P`/$destdir "$@"
-else
-    echo "LD_PRELOAD=$LD_PRELOAD"
-    HOME=/root $destdir/usr/sbin/chroot `pwd -P`/$destdir $SHELL
-fi
+$srcdir/bin/fakechroot $srcdir/chroot.sh "$@"
