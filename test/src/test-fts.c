5f0a38fe7c2b659242efdb25474876c5af491e4d
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2010-11-26 15:43:31 +0100

    Test unit for fts(3) function.

---
+++ test/src/test-fts.c
@@ -0,0 +1,47 @@
+#include <fts.h>
+
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+static int callback(const char *fpath, const struct stat *sb, int typeflag) {
+    printf("%s\n", fpath);
+    return 0;
+}
+
+
+int main (int argc, char *argv[]) {
+    FTS *tree;
+    FTSENT *node;
+
+    char **paths = argv + 1;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s path [path...]\n", argv[0]);
+        exit(2);
+    }
+
+    if ((tree = fts_open(paths, 0, 0)) == NULL) {
+        perror("fts_open");
+        exit(1);
+    }
+
+    while ((node = fts_read(tree)) != NULL) {
+        if (node->fts_info & (FTS_F|FTS_D)) {
+            printf("%s\n", node->fts_path);
+        }
+    }
+    if (errno) {
+        perror("fts_read");
+        exit(1);
+    }
+
+    if ((fts_close(tree)) == -1) {
+        perror("fts_close");
+        return 1;
+    }
+
+    return 0;
+}
---
+++ test/t/fts.t
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+srcdir=${srcdir:-.}
+. $srcdir/common.inc
+
+prepare 4
+
+for chroot in chroot fakechroot; do
+
+    if [ $chroot = "chroot" ] && ! is_root; then
+        skip $(( $tap_plan / 2 )) "not root"
+    else
+
+        mkdir -p testtree/$chroot-dir/a/b/c
+        echo "something" > testtree/$chroot-dir/a/b/c/d
+
+        t=`echo $($srcdir/$chroot.sh testtree /bin/test-fts /$chroot-dir 2>&1 | sort)`
+        test "$t" = "/$chroot-dir /$chroot-dir/a /$chroot-dir/a/b /$chroot-dir/a/b/c /$chroot-dir/a/b/c/d" || not
+        ok "$chroot fts returns" $t
+
+        t=`echo $($srcdir/$chroot.sh testtree /bin/test-fts $chroot-dir 2>&1 | sort)`
+        test "$t" = "$chroot-dir $chroot-dir/a $chroot-dir/a/b $chroot-dir/a/b/c $chroot-dir/a/b/c/d" || not
+        ok "$chroot fts returns" $t
+
+    fi
+
+done
+
+cleanup
