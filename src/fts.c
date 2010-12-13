36907f54fdf7181722f75b76c49e8c702e0cc696
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2010-12-13 23:50:51 +0100

    Do not redefine _D_ALLOC_NAMELEN if is already defined.

--- src/fts.c
+++ src/fts.c
@@ -57,16 +57,18 @@
 
 #define FTS_MAXLEVEL 0x7fff
 
-#ifdef HAVE_STRUCT_DIRENT_D_NAMLEN
-# define _D_EXACT_NAMLEN(d) ((d)->d_namlen)
-# define _D_ALLOC_NAMLEN(d) (_D_EXACT_NAMLEN (d) + 1)
-#else
-# define _D_EXACT_NAMLEN(d) (strlen ((d)->d_name))
-# ifdef HAVE_STRUCT_DIRENT_D_RECLEN
-#  define _D_ALLOC_NAMLEN(d) (((char *) (d) + (d)->d_reclen) - &(d)->d_name[0])
+#ifndef _D_ALLOC_NAMELEN
+# ifdef HAVE_STRUCT_DIRENT_D_NAMLEN
+#  define _D_EXACT_NAMLEN(d) ((d)->d_namlen)
+#  define _D_ALLOC_NAMLEN(d) (_D_EXACT_NAMLEN (d) + 1)
 # else
-#  define _D_ALLOC_NAMLEN(d) (sizeof (d)->d_name > 1 ? sizeof (d)->d_name : \
+#  define _D_EXACT_NAMLEN(d) (strlen ((d)->d_name))
+#  ifdef HAVE_STRUCT_DIRENT_D_RECLEN
+#   define _D_ALLOC_NAMLEN(d) (((char *) (d) + (d)->d_reclen) - &(d)->d_name[0])
+#  else
+#   define _D_ALLOC_NAMLEN(d) (sizeof (d)->d_name > 1 ? sizeof (d)->d_name : \
 			      _D_EXACT_NAMLEN (d) + 1)
+#  endif
 # endif
 #endif
 
