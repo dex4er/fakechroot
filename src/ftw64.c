86268463dfc5b6c535349f8b9150008c9cf41b70
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2010-12-16 22:20:59 +0100

    Reimplemented ftw based on GlibC.

--- src/ftw64.c
+++ src/ftw64.c
@@ -1,48 +1,33 @@
-/*
-    libfakechroot -- fake chroot environment
-    Copyright (c) 2010 Piotr Roszatycki <dexter@debian.org>
+/* File tree walker functions.  LFS version.
+   Copyright (C) 1996, 1997, 1998, 2001, 2006 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Ulrich Drepper <drepper@cygnus.com>, 1996.
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-*/
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
 
+#define FTW_NAME ftw64
+#define NFTW_NAME nftw64
+#define NFTW_OLD_NAME __old_nftw64
+#define NFTW_NEW_NAME __new_nftw64
+#define INO_T ino64_t
+#define STAT stat64
+#define LXSTAT __lxstat64
+#define XSTAT __xstat64
+#define FXSTATAT __fxstatat64
+#define FTW_FUNC_T __ftw64_func_t
+#define NFTW_FUNC_T __nftw64_func_t
 
-#include <config.h>
-
-#if defined(HAVE_FTW64) && !defined(OPENDIR_CALLS___OPEN) && !defined(OPENDIR_CALLS___OPENDIR2) && !defined(HAVE__XFTW64)
-
-#define _LARGEFILE64_SOURCE
-#include <sys/stat.h>
-#include "libfakechroot.h"
-
-
-static int (* ftw64_fn_saved)(const char * file, const struct stat64 * sb, int flag);
-
-static int ftw64_fn_wrapper (const char * file, const struct stat64 * sb, int flag)
-{
-    char *fakechroot_path, *fakechroot_ptr;
-    narrow_chroot_path(file, fakechroot_path, fakechroot_ptr);
-    return ftw64_fn_saved(file, sb, flag);
-}
-
-wrapper(ftw64, int, (const char * dir, int (* fn)(const char * file, const struct stat64 * sb, int flag), int nopenfd))
-{
-    char *fakechroot_path, fakechroot_buf[FAKECHROOT_PATH_MAX];
-    debug("ftw64(\"%s\", &fn, %d)", dir, nopenfd);
-    expand_chroot_path(dir, fakechroot_path, fakechroot_buf);
-    ftw64_fn_saved = fn;
-    return nextcall(ftw64)(dir, &ftw64_fn_wrapper, nopenfd);
-}
-
-#endif
+#include "ftw.c"
