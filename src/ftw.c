d4e69666425208cf4e26efa90b0bd78559877d3c
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2010-12-16 22:44:30 +0100

    Include in-place generic not-cancel.h

--- src/ftw.c
+++ src/ftw.c
@@ -70,7 +70,35 @@ char *alloca ();
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <not-cancel.h>
+
+/* By default we have none.  Map the name to the normal functions.  */
+#define open_not_cancel(name, flags, mode) \
+  __libc_open (name, flags, mode)
+#define open_not_cancel_2(name, flags) \
+  __libc_open (name, flags)
+#define openat_not_cancel(fd, name, flags, mode) \
+  __openat (fd, name, flags, mode)
+#define openat_not_cancel_3(fd, name, flags) \
+  __openat (fd, name, flags, 0)
+#define openat64_not_cancel(fd, name, flags, mode) \
+  __openat64 (fd, name, flags, mode)
+#define openat64_not_cancel_3(fd, name, flags) \
+  __openat64 (fd, name, flags, 0)
+#define close_not_cancel(fd) \
+  __close (fd)
+#define close_not_cancel_no_status(fd) \
+  (void) __close (fd)
+#define read_not_cancel(fd, buf, n) \
+  __read (fd, buf, n)
+#define write_not_cancel(fd, buf, n) \
+  __write (fd, buf, n)
+#define writev_not_cancel_no_status(fd, iov, n) \
+  (void) __writev (fd, iov, n)
+#define fcntl_not_cancel(fd, cmd, val) \
+  __fcntl (fd, cmd, val)
+# define waitpid_not_cancel(pid, stat_loc, options) \
+  __waitpid (pid, stat_loc, options)
+
 #if HAVE_SYS_PARAM_H || defined _LIBC
 # include <sys/param.h>
 #endif
