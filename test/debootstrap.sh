ff63589557320267e32e54a7fba668c2926368d4
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2011-11-26 22:56:23 +0100

    Support different variants of debootstrap. Do not set FAKECHROOT_* variables with are defined in debootstrap.env.

--- test/debootstrap.sh
+++ test/debootstrap.sh
@@ -10,6 +10,7 @@ run () {
 
 vendor=${VENDOR:-`lsb_release -s -i`}
 release=${RELEASE:-`lsb_release -s -c`}
+variant=$VARIANT
 type=`dpkg-architecture -qDEB_HOST_GNU_TYPE`
 systype=${type#*-}
 arch=${ARCH:-`dpkg-architecture -t$(arch)-$systype -qDEB_HOST_ARCH 2>/dev/null`}
@@ -21,13 +22,11 @@ else
     destdir=`pwd -P`/testtree
 fi
 
-tarball=$vendor-$release-$arch.debs.tgz
+tarball=$vendor-$release${variant:+-$variant}-$arch.debs.tgz
 
-export FAKECHROOT_EXCLUDE_PATH=${FAKECHROOT_EXCLUDE_PATH:-/dev:/proc:/sys}
-export FAKECHROOT_CMD_SUBST=/sbin/insserv=/bin/true:/sbin/ldconfig=/bin/true:/usr/bin/ischroot=/bin/true:/usr/bin/ldd=/usr/bin/ldd.fakechroot:/usr/bin/mkfifo=/bin/true
 export FAKECHROOT_AF_UNIX_PATH=/tmp
 
-debootstrap_opts="--arch=$arch --variant=fakechroot"
+debootstrap_opts="--arch=$arch ${variant:+--variant=$variant}"
 if [ ! -f $tarball ]; then
     FAKECHROOT=true fakeroot /usr/sbin/debootstrap --download-only --make-tarball=$tarball --include=build-essential,devscripts,fakeroot,gnupg $debootstrap_opts $release $destdir "$@"
 fi
