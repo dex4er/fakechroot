d6d3a851530fe57a971f742e54f57fcc93a7f1da
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2011-11-29 20:54:18 +0100

    Setup chroot wrapper for tests

--- test/chroot.sh
+++ test/chroot.sh
@@ -2,19 +2,33 @@
 
 srcdir=${srcdir:-.}
 
+pwd=`dirname $0`
+abs_top_srcdir=${abs_top_srcdir:-`cd "$pwd/../.." 2>/dev/null && pwd -P`}
+
+if ! command -v chroot >/dev/null; then
+    PATH="${PATH:-/usr/bin:/bin}:/usr/sbin:/sbin"
+    export PATH
+fi
+
+chroot=`command -v chroot 2>/dev/null`
+chroot="${chroot:-/usr/sbin/chroot}"
+
+FAKECHROOT_CMD_SUBST=$chroot=$abs_top_srcdir/scripts/chroot.fakechroot:$FAKECHROOT_CMD_SUBST
+export FAKECHROOT_CMD_SUBST
+
 if [ $# -gt 0 ]; then
     destdir=$1
     shift
 else
-    abs_srcdir=${abs_srcdir:-`cd $srcdir 2>/dev/null && pwd -P`}
+    abs_srcdir=${abs_srcdir:-`cd "$srcdir" 2>/dev/null && pwd -P`}
     destdir="$abs_srcdir/testtree"
 fi
 
 if [ $# -gt 0 ]; then
-    HOME=/root /usr/sbin/chroot $destdir "$@"
+    HOME=/root $chroot $destdir "$@"
     result=$?
 else
-    HOME=/root /usr/sbin/chroot $destdir $SHELL -i
+    HOME=/root $chroot $destdir $SHELL -i
     result=$?
 fi
 
