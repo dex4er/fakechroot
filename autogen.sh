2689d484bf258a1a879a32ca951906cf0d1cbace
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-11-08 22:38:30 +0100

    AC_CONFIG_AUX_DIR

--- autogen.sh
+++ autogen.sh
@@ -4,18 +4,26 @@
 
 set -e
 
-autogen () {
-    rm -f Makefile Makefile.in aclocal.m4 
-    rm -f config.guess config.h config.h.in config.log
-    rm -f config.status config.sub configure
-    rm -f depcomp install-sh libtool ltmain.sh missing stamp-h1
+clean () {
+    rm -f Makefile Makefile.in aclocal.m4
+    rm -f config.h config.h.in config.log
+    rm -f config.status configure
+    rm -f libtool test-driver
+    rm -f stamp-h1
     rm -f m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4
     rm -rf autom4te.cache
+    rm -rf build-aux
+}
 
+autogen () {
     autoreconf --verbose --force --install --make
 }
 
 set -x
 cd $(dirname $0)
 
+clean
+test "$1" = clean && exit 0
+
 autogen
+rm -rf autom4te.cache
