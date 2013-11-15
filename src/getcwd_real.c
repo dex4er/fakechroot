7cbd5bdc2bcbc17ea7b36bf57025c252fe1b1353
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-11-15 20:29:18 +0100

    Tabs to spaces

--- src/getcwd_real.c
+++ src/getcwd_real.c
@@ -57,38 +57,38 @@ LOCAL char * getcwd_real(char *buf, size_t size)
     size_t alloc_size = size;
 
     if (size == 0) {
-	if (buf != NULL) {
-	    __set_errno(EINVAL);
-	    return NULL;
-	}
-	alloc_size = MAX (PATH_MAX, getpagesize ());
+        if (buf != NULL) {
+            __set_errno(EINVAL);
+            return NULL;
+        }
+        alloc_size = MAX (PATH_MAX, getpagesize ());
     }
     path=buf;
     if (buf == NULL) {
-	path = malloc(alloc_size);
-	if (path == NULL)
-	    return NULL;
+        path = malloc(alloc_size);
+        if (path == NULL)
+            return NULL;
     }
     ret = syscall(SYS_getcwd, path, alloc_size);
     if (ret >= 0)
     {
-	if (buf == NULL && size == 0)
-	    buf = realloc(path, ret);
-	if (buf == NULL)
-	    buf = path;
-	return buf;
+        if (buf == NULL && size == 0)
+            buf = realloc(path, ret);
+        if (buf == NULL)
+            buf = path;
+        return buf;
     }
     if (buf == NULL)
-	free (path);
+        free (path);
     return NULL;
 }
 
 #else
 
-/*	$OpenBSD: getcwd.c,v 1.14 2005/08/08 08:05:34 espie Exp $ */
+/*      $OpenBSD: getcwd.c,v 1.14 2005/08/08 08:05:34 espie Exp $ */
 /*
  * Copyright (c) 1989, 1991, 1993
- *	The Regents of the University of California.  All rights reserved.
+ *      The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -141,209 +141,209 @@ LOCAL char * getcwd_real(char *buf, size_t size)
 # define NAMLEN(d) (strlen ((d)->d_name))
 #endif
 
-#define	ISDOT(dp) \
-	(dp->d_name[0] == '.' && (dp->d_name[1] == '\0' || \
-	    (dp->d_name[1] == '.' && dp->d_name[2] == '\0')))
+#define ISDOT(dp) \
+        (dp->d_name[0] == '.' && (dp->d_name[1] == '\0' || \
+            (dp->d_name[1] == '.' && dp->d_name[2] == '\0')))
 
 LOCAL char * getcwd_real(char *pt, size_t size)
 {
-	struct dirent *dp;
-	DIR *dir = NULL;
-	dev_t dev;
-	ino_t ino;
-	int first;
-	char *bpt, *bup;
+        struct dirent *dp;
+        DIR *dir = NULL;
+        dev_t dev;
+        ino_t ino;
+        int first;
+        char *bpt, *bup;
 #ifdef HAVE___LXSTAT64
     struct stat64 s;
 #else
     struct stat s;
 #endif
-	dev_t root_dev;
-	ino_t root_ino;
-	size_t ptsize, upsize;
-	int save_errno;
-	char *ept, *eup, *up;
+        dev_t root_dev;
+        ino_t root_ino;
+        size_t ptsize, upsize;
+        int save_errno;
+        char *ept, *eup, *up;
 
     debug("getcwd_real(&pt, %d)", size);
 
     /*
-	 * If no buffer specified by the user, allocate one as necessary.
-	 * If a buffer is specified, the size has to be non-zero.  The path
-	 * is built from the end of the buffer backwards.
-	 */
-	if (pt) {
-		ptsize = 0;
-		if (!size) {
-			errno = EINVAL;
-			return (NULL);
-		}
-		ept = pt + size;
-	} else {
-		if ((pt = malloc(ptsize = MAXPATHLEN)) == NULL)
+         * If no buffer specified by the user, allocate one as necessary.
+         * If a buffer is specified, the size has to be non-zero.  The path
+         * is built from the end of the buffer backwards.
+         */
+        if (pt) {
+                ptsize = 0;
+                if (!size) {
+                        errno = EINVAL;
+                        return (NULL);
+                }
+                ept = pt + size;
+        } else {
+                if ((pt = malloc(ptsize = MAXPATHLEN)) == NULL)
             debug("getcwd_real(NULL, %d)", pt, size);
-			return (NULL);
-		ept = pt + ptsize;
-	}
-	bpt = ept - 1;
-	*bpt = '\0';
+                        return (NULL);
+                ept = pt + ptsize;
+        }
+        bpt = ept - 1;
+        *bpt = '\0';
 
-	/*
-	 * Allocate bytes for the string of "../"'s.
-	 * Should always be enough (it's 340 levels).  If it's not, allocate
-	 * as necessary.  Special * case the first stat, it's ".", not "..".
-	 */
-	if ((up = malloc(upsize = MAXPATHLEN)) == NULL)
-		goto err;
-	eup = up + upsize;
-	bup = up;
-	up[0] = '.';
-	up[1] = '\0';
+        /*
+         * Allocate bytes for the string of "../"'s.
+         * Should always be enough (it's 340 levels).  If it's not, allocate
+         * as necessary.  Special * case the first stat, it's ".", not "..".
+         */
+        if ((up = malloc(upsize = MAXPATHLEN)) == NULL)
+                goto err;
+        eup = up + upsize;
+        bup = up;
+        up[0] = '.';
+        up[1] = '\0';
 
-	/* Save root values, so know when to stop. */
-	if (LSTAT("/", &s))
-		goto err;
-	root_dev = s.st_dev;
-	root_ino = s.st_ino;
+        /* Save root values, so know when to stop. */
+        if (LSTAT("/", &s))
+                goto err;
+        root_dev = s.st_dev;
+        root_ino = s.st_ino;
 
-	errno = 0;			/* XXX readdir has no error return. */
+        errno = 0;                      /* XXX readdir has no error return. */
 
-	for (first = 1;; first = 0) {
-		/* Stat the current level. */
-		if (LSTAT(up, &s))
+        for (first = 1;; first = 0) {
+                /* Stat the current level. */
+                if (LSTAT(up, &s))
             goto err;
 
-		/* Save current node values. */
-		ino = s.st_ino;
-		dev = s.st_dev;
-		/* Check for reaching root. */
-		if (root_dev == dev && root_ino == ino) {
-			*--bpt = '/';
-			/*
-			 * It's unclear that it's a requirement to copy the
-			 * path to the beginning of the buffer, but it's always
-			 * been that way and stuff would probably break.
-			 */
-			memmove(pt, bpt, ept - bpt);
-			free(up);
-		    debug("getcwd_real(\"%s\", %d)", pt, size);
-			return (pt);
-		}
+                /* Save current node values. */
+                ino = s.st_ino;
+                dev = s.st_dev;
+                /* Check for reaching root. */
+                if (root_dev == dev && root_ino == ino) {
+                        *--bpt = '/';
+                        /*
+                         * It's unclear that it's a requirement to copy the
+                         * path to the beginning of the buffer, but it's always
+                         * been that way and stuff would probably break.
+                         */
+                        memmove(pt, bpt, ept - bpt);
+                        free(up);
+                    debug("getcwd_real(\"%s\", %d)", pt, size);
+                        return (pt);
+                }
 
-		/*
-		 * Build pointer to the parent directory, allocating memory
-		 * as necessary.  Max length is 3 for "../", the largest
-		 * possible component name, plus a trailing NUL.
-		 */
-		if (bup + 3  + MAXNAMLEN + 1 >= eup) {
-			char *nup;
+                /*
+                 * Build pointer to the parent directory, allocating memory
+                 * as necessary.  Max length is 3 for "../", the largest
+                 * possible component name, plus a trailing NUL.
+                 */
+                if (bup + 3  + MAXNAMLEN + 1 >= eup) {
+                        char *nup;
 
-			if ((nup = realloc(up, upsize *= 2)) == NULL)
-				goto err;
-			bup = nup + (bup - up);
-			up = nup;
-			eup = up + upsize;
-		}
-		*bup++ = '.';
-		*bup++ = '.';
-		*bup = '\0';
+                        if ((nup = realloc(up, upsize *= 2)) == NULL)
+                                goto err;
+                        bup = nup + (bup - up);
+                        up = nup;
+                        eup = up + upsize;
+                }
+                *bup++ = '.';
+                *bup++ = '.';
+                *bup = '\0';
 
-		/* Open and stat parent directory. */
+                /* Open and stat parent directory. */
 #ifdef HAVE___FXSTAT64
-		if (!(dir = nextcall(opendir)(up)) || __fxstat64(_STAT_VER, dirfd(dir), &s))
-			goto err;
+                if (!(dir = nextcall(opendir)(up)) || __fxstat64(_STAT_VER, dirfd(dir), &s))
+                        goto err;
 #else
         if (!(dir = opendir(up)) || fstat(dirfd(dir), &s))
             goto err;
 #endif
 
-		/* Add trailing slash for next directory. */
-		*bup++ = '/';
+                /* Add trailing slash for next directory. */
+                *bup++ = '/';
 
-		/*
-		 * If it's a mount point, have to stat each element because
-		 * the inode number in the directory is for the entry in the
-		 * parent directory, not the inode number of the mounted file.
-		 */
-		save_errno = 0;
-		if (s.st_dev == dev) {
-			for (;;) {
-				if (!(dp = readdir(dir)))
-					goto notfound;
-				if (dp->d_fileno == ino)
-					break;
-			}
-		} else
-			for (;;) {
-				if (!(dp = readdir(dir)))
-					goto notfound;
-				if (ISDOT(dp))
-					continue;
-				memcpy(bup, dp->d_name, NAMLEN(dp) + 1);
+                /*
+                 * If it's a mount point, have to stat each element because
+                 * the inode number in the directory is for the entry in the
+                 * parent directory, not the inode number of the mounted file.
+                 */
+                save_errno = 0;
+                if (s.st_dev == dev) {
+                        for (;;) {
+                                if (!(dp = readdir(dir)))
+                                        goto notfound;
+                                if (dp->d_fileno == ino)
+                                        break;
+                        }
+                } else
+                        for (;;) {
+                                if (!(dp = readdir(dir)))
+                                        goto notfound;
+                                if (ISDOT(dp))
+                                        continue;
+                                memcpy(bup, dp->d_name, NAMLEN(dp) + 1);
 
-				/* Save the first error for later. */
-				if (LSTAT(up, &s)) {
-					if (!save_errno)
-						save_errno = errno;
-					errno = 0;
-					continue;
-				}
-				if (s.st_dev == dev && s.st_ino == ino)
-					break;
-			}
+                                /* Save the first error for later. */
+                                if (LSTAT(up, &s)) {
+                                        if (!save_errno)
+                                                save_errno = errno;
+                                        errno = 0;
+                                        continue;
+                                }
+                                if (s.st_dev == dev && s.st_ino == ino)
+                                        break;
+                        }
 
-		/*
-		 * Check for length of the current name, preceding slash,
-		 * leading slash.
-		 */
-		if (bpt - pt < NAMLEN(dp) + (first ? 1 : 2)) {
-			size_t len;
-			char *npt;
+                /*
+                 * Check for length of the current name, preceding slash,
+                 * leading slash.
+                 */
+                if (bpt - pt < NAMLEN(dp) + (first ? 1 : 2)) {
+                        size_t len;
+                        char *npt;
 
-			if (!ptsize) {
-				errno = ERANGE;
-				goto err;
-			}
-			len = ept - bpt;
-			if ((npt = realloc(pt, ptsize *= 2)) == NULL)
-				goto err;
-			bpt = npt + (bpt - pt);
-			pt = npt;
-			ept = pt + ptsize;
-			memmove(ept - len, bpt, len);
-			bpt = ept - len;
-		}
-		if (!first)
-			*--bpt = '/';
-		bpt -= NAMLEN(dp);
-		memcpy(bpt, dp->d_name, NAMLEN(dp));
-		(void)closedir(dir);
+                        if (!ptsize) {
+                                errno = ERANGE;
+                                goto err;
+                        }
+                        len = ept - bpt;
+                        if ((npt = realloc(pt, ptsize *= 2)) == NULL)
+                                goto err;
+                        bpt = npt + (bpt - pt);
+                        pt = npt;
+                        ept = pt + ptsize;
+                        memmove(ept - len, bpt, len);
+                        bpt = ept - len;
+                }
+                if (!first)
+                        *--bpt = '/';
+                bpt -= NAMLEN(dp);
+                memcpy(bpt, dp->d_name, NAMLEN(dp));
+                (void)closedir(dir);
 
-		/* Truncate any file name. */
-		*bup = '\0';
-	}
+                /* Truncate any file name. */
+                *bup = '\0';
+        }
 
 notfound:
-	/*
-	 * If readdir set errno, use it, not any saved error; otherwise,
-	 * didn't find the current directory in its parent directory, set
-	 * errno to ENOENT.
-	 */
-	if (!errno)
-		errno = save_errno ? save_errno : ENOENT;
-	/* FALLTHROUGH */
+        /*
+         * If readdir set errno, use it, not any saved error; otherwise,
+         * didn't find the current directory in its parent directory, set
+         * errno to ENOENT.
+         */
+        if (!errno)
+                errno = save_errno ? save_errno : ENOENT;
+        /* FALLTHROUGH */
 err:
-	save_errno = errno;
+        save_errno = errno;
 
-	if (ptsize)
-		free(pt);
-	free(up);
-	if (dir)
-		(void)closedir(dir);
+        if (ptsize)
+                free(pt);
+        free(up);
+        if (dir)
+                (void)closedir(dir);
 
-	errno = save_errno;
+        errno = save_errno;
 
     debug("getcwd_real(NULL, %d)", pt, size);
-	return (NULL);
+        return (NULL);
 }
 
 #endif
--- src/strlcpy.c
+++ src/strlcpy.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: strlcpy.c,v 1.10 2005/08/08 08:05:37 espie Exp $	*/
+/*      $OpenBSD: strlcpy.c,v 1.10 2005/08/08 08:05:37 espie Exp $      */
 
 /*
  * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -35,27 +35,27 @@
 LOCAL size_t
 strlcpy(char *dst, const char *src, size_t siz)
 {
-	char *d = dst;
-	const char *s = src;
-	size_t n = siz;
+        char *d = dst;
+        const char *s = src;
+        size_t n = siz;
 
-	/* Copy as many bytes as will fit */
-	if (n != 0) {
-		while (--n != 0) {
-			if ((*d++ = *s++) == '\0')
-				break;
-		}
-	}
+        /* Copy as many bytes as will fit */
+        if (n != 0) {
+                while (--n != 0) {
+                        if ((*d++ = *s++) == '\0')
+                                break;
+                }
+        }
 
-	/* Not enough room in dst, add NUL and traverse rest of src */
-	if (n == 0) {
-		if (siz != 0)
-			*d = '\0';		/* NUL-terminate dst */
-		while (*s++)
-			;
-	}
+        /* Not enough room in dst, add NUL and traverse rest of src */
+        if (n == 0) {
+                if (siz != 0)
+                        *d = '\0';              /* NUL-terminate dst */
+                while (*s++)
+                        ;
+        }
 
-	return(s - src - 1);	/* count does not include NUL */
+        return(s - src - 1);    /* count does not include NUL */
 }
 
 #endif
