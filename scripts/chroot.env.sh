43b96bb8c36932fa2d6cd61e208f7e7ab79ef24f
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-10-16 11:05:44 +0200

    More safe wrappers (bindir can contain space)

--- scripts/chroot.env.sh
+++ scripts/chroot.env.sh
@@ -10,14 +10,14 @@ fi
 fakechroot_chroot_env_cmd_subst=""
 for fakechroot_chroot_env_d in `echo $PATH | tr ':' ' '`; do
     fakechroot_chroot_env_cmd_subst="$fakechroot_chroot_env_cmd_subst
-        $fakechroot_chroot_env_d/chroot=${fakechroot_bindir:-@sbindir@}/chroot.fakechroot
-        $fakechroot_chroot_env_d/env=${fakechroot_bindir:-@bindir@}/env.fakechroot
-        $fakechroot_chroot_env_d/ischroot=/bin/true
-        $fakechroot_chroot_env_d/ldconfig=/bin/true
-        $fakechroot_chroot_env_d/ldd=${fakechroot_bindir:-@bindir@}/ldd.fakechroot
-    "
+$fakechroot_chroot_env_d/chroot=${fakechroot_bindir:-@sbindir@}/chroot.fakechroot
+$fakechroot_chroot_env_d/env=${fakechroot_bindir:-@bindir@}/env.fakechroot
+$fakechroot_chroot_env_d/ischroot=/bin/true
+$fakechroot_chroot_env_d/ldconfig=/bin/true
+$fakechroot_chroot_env_d/ldd=${fakechroot_bindir:-@bindir@}/ldd.fakechroot"
 done
-FAKECHROOT_CMD_SUBST="${FAKECHROOT_CMD_SUBST:+$FAKECHROOT_CMD_SUBST:}`echo $fakechroot_chroot_env_cmd_subst | tr ' ' ':'`"
+
+FAKECHROOT_CMD_SUBST="${FAKECHROOT_CMD_SUBST:+$FAKECHROOT_CMD_SUBST:}`echo \"$fakechroot_chroot_env_cmd_subst\" | tr '\012' ':'`"
 export FAKECHROOT_CMD_SUBST
 
 # Set the default list of directories excluded from being chrooted
--- scripts/rinse.env.sh
+++ scripts/rinse.env.sh
@@ -7,19 +7,18 @@ if ! command -v chroot >/dev/null; then
 fi
 
 # Set a list of command substitutions needed by rinse
-fakechroot_rinse_env_cmd_subst="
-    /bin/mount=/bin/true
-    /sbin/ldconfig=/bin/true
-    /sbin/new-kernel-pkg=/bin/true
-    /usr/bin/ischroot=/bin/true
-    /usr/bin/ldd=${fakechroot_bindir:-@bindir@}/ldd.fakechroot
-    /usr/sbin/build-locale-archive=/bin/true
-    /usr/sbin/glibc_post_upgrade.i686=/bin/true
-    /usr/sbin/glibc_post_upgrade.x86_64=/bin/true
-    /usr/sbin/libgcc_post_upgrade=/bin/true
-    /usr/sbin/nscd=/bin/true
-"
-FAKECHROOT_CMD_SUBST="${FAKECHROOT_CMD_SUBST:+$FAKECHROOT_CMD_SUBST:}$(echo $fakechroot_rinse_env_cmd_subst | tr ' ' ':')"
+fakechroot_rinse_env_cmd_subst="/bin/mount=/bin/true
+/sbin/ldconfig=/bin/true
+/sbin/new-kernel-pkg=/bin/true
+/usr/bin/ischroot=/bin/true
+/usr/bin/ldd=${fakechroot_bindir:-@bindir@}/ldd.fakechroot
+/usr/sbin/build-locale-archive=/bin/true
+/usr/sbin/glibc_post_upgrade.i686=/bin/true
+/usr/sbin/glibc_post_upgrade.x86_64=/bin/true
+/usr/sbin/libgcc_post_upgrade=/bin/true
+/usr/sbin/nscd=/bin/true"
+
+FAKECHROOT_CMD_SUBST="${FAKECHROOT_CMD_SUBST:+$FAKECHROOT_CMD_SUBST:}`echo "$fakechroot_rinse_env_cmd_subst" | tr '\012' ':'`"
 
 # Set the default list of directories excluded from being chrooted
 FAKECHROOT_EXCLUDE_PATH="${FAKECHROOT_EXCLUDE_PATH:-/dev:/proc:/sys}"
